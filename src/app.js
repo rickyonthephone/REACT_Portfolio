4a8fc9d3c38b2cc2282588960a487695881ed85c
Richard Crippen <richard.crippen@gmail.com>
2021-07-06 21:58:29 -0400

    adding base react files and folders

---
+++ src/app.js
@@ -0,0 +1,12 @@
+// to add CSS if desired create CSS file and import it here using "import './App.css';""
+// import component here  - example "import BucketList from './components/BucketList';""
+
+function App() {
+  return (
+    <div className="bucket-app">
+      {/* add component here <BucketList /> */}
+    </div>
+  );
+}
+//export to use elsewhere
+export default App;
