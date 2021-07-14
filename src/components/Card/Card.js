8f46e093eee7816b85290abb09d33084ffef24bd
Richard Crippen <richard.crippen@gmail.com>
2021-07-14 12:26:02 -0400

    adding form fill to contact page

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import "./Card.css"
 
 const Card = ({ title, description, screenShot, url, deployment, deploymentUrl}) => {
 
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,25 +1,28 @@
 import {Form, Button} from 'react-bootstrap';
+import './Form.css';
 
 
 function ContactForm(){
     return(
-        <Form>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
+        <div className='formWrapper'>
+        <Form className='contactForm'>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        Your contact information will be kept private.
+                    </Form.Text>
+            </Form.Group>
 
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Regarding:</Form.Label>
+                <Form.Control type="about" placeholder="What project would you like to start?" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+        </div>
     )
 }
 
